7822604a76928cb74ae1a58826f6f6d4b72f967f
biu <hezhou920@gmail.com>
2023-01-16 18:26:00 +0800

    Guess the number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
+mod guess_number;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rust demo 正在运行...");
+
+    loop {
+        println!();
+        println!("请选择要执行的程序:");
+        println!("1. 猜数字");
+        println!();
+
+        let mut number = String::new();
+
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to read line");
+
+        println!();
+
+        match number.trim() {
+            "1" => guess_number::guess_number(),
+            _ => println!("没有该程序"),
+        }
+    }
 }
