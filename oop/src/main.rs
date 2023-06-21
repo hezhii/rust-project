c5913dac15fd31d4cd43ed707ace30034251f867
biu <hezhou920@gmail.com>
2023-06-21 16:35:57 +0800

    oop

---
+++ oop/src/main.rs
@@ -0,0 +1,42 @@
+mod ac;
+use oop::{Button, Draw, Screen};
+
+fn main() {
+    let mut collection = ac::AveragedCollection::new(vec![2, 3]);
+
+    collection.add(1);
+    println!("average: {}", collection.average());
+
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
